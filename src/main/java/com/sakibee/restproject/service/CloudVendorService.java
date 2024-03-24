554bfd8f7859208dd4b7049c542f7a9f64bff5a4
SakiBee <hi.sakib75@gmail.com>
2024-03-24 11:39:18 +0600

    add CloudVendorService class

---
+++ src/main/java/com/sakibee/restproject/service/CloudVendorService.java
@@ -0,0 +1,13 @@
+package com.sakibee.restproject.service;
+
+import com.sakibee.restproject.model.CloudVendor;
+
+import java.util.List;
+
+public interface CloudVendorService {
+    public String createCloudVendor(CloudVendor cloudVendor);
+    public String updateCloudVendor(CloudVendor cloudVendor);
+    public String deleteCloudVendor(String cloudVendorId);
+    public CloudVendor getCloudVendor(String cloudVendorId);
+    public List<CloudVendor> getAllCloudVendors();
+}
