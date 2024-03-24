cadd367aa325dc920e6b7700778e83ba4413a73f
SakiBee <hi.sakib75@gmail.com>
2024-03-24 11:39:37 +0600

    add CloudVendorRepository class

---
+++ src/main/java/com/sakibee/restproject/repository/CloudVendorRepository.java
@@ -0,0 +1,6 @@
+package com.sakibee.restproject.repository;
+
+import com.sakibee.restproject.model.CloudVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {}
